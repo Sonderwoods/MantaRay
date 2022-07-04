0e2b78d7bc8a806a7aae59c22887ced42c06c1cc
Mathias Sønderskov Schaltz <sonderskovmathias@gmail.com>
2022-07-04 22:37:43 +0200

    moved

--- GrasshopperRadianceLinuxConnector/OldComponents/GH_ExecuteOld.cs
+++ GrasshopperRadianceLinuxConnector/OldComponents/GH_ExecuteOld.cs
@@ -11,14 +11,14 @@ using Rhino.Geometry;
 namespace GrasshopperRadianceLinuxConnector
 {
     [Obsolete]
-    public class GH_Execute : GH_Template
+    public class GH_ExecuteOld : GH_Template
     {
         
         /// <summary>
         /// Initializes a new instance of the GH_Execute class.
         /// </summary>
         ///
-        public GH_Execute()
+        public GH_ExecuteOld()
           : base("Execute SSH", "Execute SSH",
               "Use me to execute a SSH Command",
               "1 SSH")
