ea2980cbe5fb29fa071e5a4a341ad53c4165c193
Mathias Sønderskov Schaltz <sonderskovmathias@gmail.com>
2022-07-06 17:04:07 +0200

    test

---
+++ GrasshopperRadianceLinuxConnector/Components/Templates/GH_TestComponentColor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace GrasshopperRadianceLinuxConnector.Components.Templates
+{
+    public class GH_TestComponentColor : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the GH_TestComponentColor class.
+        /// </summary>
+        public GH_TestComponentColor()
+          : base("GH_TestComponentColor", "Nickname",
+              "Description",
+              "Category", "Subcategory")
+        {
+        }
+
+        public override void CreateAttributes()
+        {
+            //base.CreateAttributes();
+            m_attributes = new GH_TestComponentColor_Attr(this);
+
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddColourParameter("Color", "Col", "Col", GH_ParamAccess.item);
+            pManager.AddColourParameter("SelColor", "SelCol", "Col", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            ((GH_TestComponentColor_Attr)m_attributes).Color = DA.Fetch<System.Drawing.Color>("Color");
+            ((GH_TestComponentColor_Attr)m_attributes).ColorSelected = DA.Fetch<System.Drawing.Color>("SelColor");
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("6A4DEF5C-7EB4-4490-B233-3C630BED68D1"); }
+        }
+    }
+}
---
+++ GrasshopperRadianceLinuxConnector/Components/Templates/GH_TestComponentColor_Attr.cs
@@ -0,0 +1,63 @@
+﻿using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrasshopperRadianceLinuxConnector
+{
+    /// <summary>
+    /// This class sets colors for the grasshopper components
+    /// https://discourse.mcneel.com/t/change-the-color-of-the-custom-component/56435/2
+    /// </summary>
+    public class GH_TestComponentColor_Attr : GH_ComponentAttributes
+    {
+
+        public GH_TestComponentColor_Attr(IGH_Component component)
+          : base(component)
+        {
+
+        }
+
+        public System.Drawing.Color ColorSelected { get; set; } = System.Drawing.Color.White;
+        public System.Drawing.Color Color { get; set; } = System.Drawing.Color.Black;
+
+
+
+        /// <summary>
+        /// Renders the running components in another color
+        /// </summary>
+        /// <param name="canvas"></param>
+        /// <param name="graphics"></param>
+        /// <param name="channel"></param>
+        protected override void Render(GH_Canvas canvas, Graphics graphics, GH_CanvasChannel channel)
+        {
+
+            // Cache the existing style.
+            GH_PaletteStyle style = GH_Skin.palette_normal_standard;
+            GH_PaletteStyle selectedStyle = GH_Skin.palette_normal_selected;
+            GH_PaletteStyle styleHidden = GH_Skin.palette_hidden_standard;
+            GH_PaletteStyle styleHiddenSelected = GH_Skin.palette_hidden_selected;
+
+            // Swap out palette for normal, unselected components.
+            GH_Skin.palette_normal_standard = new GH_PaletteStyle(Color);
+            GH_Skin.palette_hidden_standard = new GH_PaletteStyle(Color);
+
+            GH_Skin.palette_normal_selected = new GH_PaletteStyle(ColorSelected);
+            GH_Skin.palette_hidden_selected = new GH_PaletteStyle(ColorSelected);
+
+            base.Render(canvas, graphics, channel);
+
+            // Put the original style back.
+            GH_Skin.palette_normal_standard = style;
+            GH_Skin.palette_normal_selected = selectedStyle;
+            GH_Skin.palette_hidden_standard = styleHidden;
+            GH_Skin.palette_hidden_selected = styleHiddenSelected;
+
+        }
+    }
+}
