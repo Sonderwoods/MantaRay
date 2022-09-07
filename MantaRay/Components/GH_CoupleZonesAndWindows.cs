3d6222edbffb0ecfba1fc203a45aec83789bf249
Mathias Sønderskov Schaltz <sonderskovmathias@gmail.com>
2022-09-07 22:25:51 +0200

    fixes

--- MantaRay/Components/GH_CoupleZonesAndWindows.cs
+++ MantaRay/Components/GH_CoupleZonesAndWindows.cs
@@ -11,13 +11,17 @@ using MantaRay.Components;
 using MantaRay.Helpers;
 using Rhino.Display;
 using Rhino.Geometry;
+using Rhino.Geometry.Collections;
 
 namespace MantaRay.Components
 {
     public class GH_CoupleZonesAndWindows : GH_Template
     {
-        List<System.Drawing.Color> lineColors = new List<Color>();
-        List<Line> lines = new List<Line>();
+        readonly List<Color> lineColors = new List<Color>();
+        readonly List<Line> lines = new List<Line>();
+        readonly List<Line> normalLines = new List<Line>();
+        readonly List<Color> normalColors = new List<Color>();
+        readonly List<Tuple<Plane, Color, string>> textTags = new List<Tuple<Plane, Color, string>>();
         BoundingBox bb = BoundingBox.Empty;
         Mesh[][] previewRooms = new Mesh[0][];
         DisplayMaterial[][] previewRoomMaterials = new DisplayMaterial[0][];
@@ -37,6 +41,7 @@ namespace MantaRay.Components
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddBrepParameter("Window surfaces", "Window surfaces", "Breps representing the windows.", GH_ParamAccess.tree);
+            pManager[pManager.AddTextParameter("Window Names", "Window Names", "Names", GH_ParamAccess.tree, "name")].Optional = true;
             pManager.AddBrepParameter("Simulation Surface", "Simulation Surfaces", "A brep representing the zone. (like a flat one representing the floor)\n" +
                 "We will use Brep.NearestPoint to couple it with windows.\n" +
                 "If you have tall windows you could 'tweak' your brep and add height to it etc.\n" +
@@ -55,8 +60,8 @@ namespace MantaRay.Components
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            int a = pManager.AddBrepParameter("Window Surface", "Window Surface", "Windows", GH_ParamAccess.tree);
             int b = pManager.AddBrepParameter("Simulation Surface", "Simulation Surface", "Rooms", GH_ParamAccess.tree);
+            int a = pManager.AddBrepParameter("Window Surface", "Window Surface", "Windows", GH_ParamAccess.tree);
             pManager.AddTextParameter("Paths per window", "Window Paths", "Paths per window.", GH_ParamAccess.list);
 
             pManager.HideParameter(a);
@@ -70,33 +75,42 @@ namespace MantaRay.Components
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             var grids = DA.FetchTree<GH_Brep>(0);
-            var windows = DA.FetchTree<GH_Brep>(1);
+            var names = DA.FetchTree<GH_String>(1);
+            var windows = DA.FetchTree<GH_Brep>(2);
             double tol1 = DA.Fetch<double>("tol1");
             double tol2 = DA.Fetch<double>("tol2");
 
             if (tol1 == 0)
                 tol1 = 0.1.FromMeter();
 
-            if (grids.PathCount != windows.PathCount)
+            if (grids.PathCount != windows.PathCount || (grids.PathCount != names.PathCount && names.PathCount > 1))
                 throw new Exception("Path counts do not match");
 
             GH_Structure<GH_Brep> outGrids = new GH_Structure<GH_Brep>();
             GH_Structure<GH_Brep> outWindows = new GH_Structure<GH_Brep>();
             GH_Structure<GH_String> outPaths = new GH_Structure<GH_String>();
+            GH_Structure<GH_String> outNames = new GH_Structure<GH_String>();
 
             lines.Clear();
             lineColors.Clear();
+            normalLines.Clear();
+            normalColors.Clear();
             previewRooms = new Mesh[grids.PathCount][];
             previewRoomMaterials = new DisplayMaterial[grids.PathCount][];
 
             previewWindows = new Mesh[grids.PathCount][];
             previewWindowMaterials = new DisplayMaterial[grids.PathCount][];
 
+            textTags.Clear();
+
+            var name = names.get_FirstItem(false) ?? new GH_String("name");
+
 
             for (int i = 0; i < grids.PathCount; i++)
             {
+                List<GH_String> _names = (List<GH_String>)names.get_Branch(i) ?? new List<GH_String>() { name };
 
-                List<GH_Brep> _grids = ((List<GH_Brep>)grids.get_Branch(i));
+                List<GH_Brep> _grids = (List<GH_Brep>)grids.get_Branch(i);
 
                 List<GH_Brep> _windows = (List<GH_Brep>)windows.get_Branch(i);
 
@@ -110,10 +124,10 @@ namespace MantaRay.Components
                 Point3d[] windowPts = _windows.Select(w => AreaMassProperties.Compute(w.Value, true, true, false, false).Centroid).ToArray();
                 Point3d[] roomPts = _grids.Select(w => AreaMassProperties.Compute(w.Value, true, true, false, false).Centroid).ToArray();
 
-                int[] roomsPerWindow = Helpers.RTreeHelper.ConnectPointsToBreps(windowPts, _grids.Select(g => g.Value).ToList(), tol1, tol2, false);
+                int[] roomsPerWindow = RTreeHelper.ConnectPointsToBreps(windowPts, _grids.Select(g => g.Value).ToList(), tol1, tol2, false);
 
-                List<Point3d> roomCenters = new List<Point3d>();
-                List<List<Point3d>> windowCentersPerRoom = new List<List<Point3d>>();
+                //List<Point3d> roomCenters = new List<Point3d>();
+                //List<List<Point3d>> windowCentersPerRoom = new List<List<Point3d>>();
 
 
 
@@ -121,8 +135,8 @@ namespace MantaRay.Components
                 {
                     GH_Path gridPath = new GH_Path(grids.Paths[i]).AppendElement(j);
                     outGrids.Append(_grids[j], gridPath);
-                    roomCenters.Add(roomPts[j]);
-                    windowCentersPerRoom.Add(new List<Point3d>());
+                    //roomCenters.Add(roomPts[j]);
+                    //windowCentersPerRoom.Add(new List<Point3d>());
                     previewRooms[i][j] = Mesh.CreateFromBrep(_grids[j].Value, MeshingParameters.Default).FirstOrDefault();
                     previewRoomMaterials[i][j] = new DisplayMaterial(ColorHelper.GetRandomColor(), 0);
 
@@ -134,31 +148,62 @@ namespace MantaRay.Components
                 {
                     GH_Path windowPath = new GH_Path(grids.Paths[i]).AppendElement(roomsPerWindow[j]);
                     outWindows.Append(_windows[j], windowPath);
+                    outNames.Append(_names[j], windowPath);
 
                     GH_Path windowPathPath = new GH_Path(grids.Paths[i]);
                     outPaths.Append(new GH_String(windowPath.ToString()), windowPathPath);
 
                     lines.Add(new Line(windowPts[j], roomPts[roomsPerWindow[j]]));
 
+                    Color Darkcolor = Color.FromArgb(
+                            previewRoomMaterials[i][roomsPerWindow[j]].Diffuse.R - 80,
+                            previewRoomMaterials[i][roomsPerWindow[j]].Diffuse.G - 80,
+                            previewRoomMaterials[i][roomsPerWindow[j]].Diffuse.B - 80);
+
+                    Point3d center = default;
+                    foreach (var vertex in _windows[j].Value.Vertices)
+                    {
+                        center += vertex.Location;
+                    }
+                    center /= _windows[j].Value.Vertices.Count;
+
+                    Vector3d normal = _windows[j].Value.Faces[0].NormalAt(0.5, 0.5);
+
+                    Vector3d vectorUpOrDown = center - _windows[j].Value.Faces[0].PointAt(0.5, 0.6);
+
+                    Vector3d flatNormalRotated = new Vector3d(normal.Y, -normal.X, 0);
+
+                    textTags.Add(new Tuple<Plane, Color, string>(
+                        new Plane(center, flatNormalRotated, vectorUpOrDown.Z > 0 ? vectorUpOrDown : -vectorUpOrDown),
+                        Darkcolor,
+                        _names[_names.Count > j ? j : 0].Value));
 
 
-                    double dotProduct = lines[lines.Count - 1].Direction * _windows[j].Value.Faces[0].NormalAt(0.5, 0.5);
-                    if (dotProduct > 0.0)
+                    
+          
+
+                    normalLines.Add(new Line(center, center + 0.5.FromMeter() * normal));
+
+                    bool isParallelish = lines[lines.Count - 1].Direction * normal > 0.0;
+                    if (isParallelish)
                     {
                         lineColors.Add(previewRoomMaterials[i][roomsPerWindow[j]].Diffuse);
+                        normalColors.Add(Darkcolor);
 
                     }
                     else
                     {
-                        lineColors.Add(System.Drawing.Color.Red);
+                        lineColors.Add(Color.Red);
+                        normalColors.Add(Color.Black);
 
+                        //Add outline of the brep also.
                         foreach (BrepEdge edge in _windows[j].Value.Edges)
                         {
                             Polyline vertices = edge.ToPolyline(DocumentTolerance(), DocumentAngleTolerance(), 0.1, double.MaxValue).ToPolyline();
                             for (int k = 0; k < vertices.Count - 1; k++)
                             {
                                 lines.Add(new Line(vertices[k], vertices[k + 1]));
-                                lineColors.Add(System.Drawing.Color.Red);
+                                lineColors.Add(Color.Red);
                             }
                         }
                     }
@@ -166,7 +211,14 @@ namespace MantaRay.Components
 
 
                     previewWindows[i][j] = Mesh.CreateFromBrep(_windows[j].Value, MeshingParameters.Default).FirstOrDefault();
-                    previewWindowMaterials[i][j] = new DisplayMaterial(previewRoomMaterials[i][roomsPerWindow[j]]) { Transparency = 0.2, IsTwoSided = true, BackDiffuse = Color.Black };
+                    previewWindowMaterials[i][j] = new DisplayMaterial()
+                    {
+                        Transparency = 0.3,
+                        BackTransparency = 0.3,
+                        IsTwoSided = true,
+                        BackDiffuse = isParallelish ? Color.Green : previewRoomMaterials[i][roomsPerWindow[j]].Diffuse,
+                        Diffuse = isParallelish ? previewRoomMaterials[i][roomsPerWindow[j]].Diffuse : Color.Red
+                    };
 
 
 
@@ -196,8 +248,22 @@ namespace MantaRay.Components
 
         public override void DrawViewportMeshes(IGH_PreviewArgs args)
         {
+
+            double threshold = 35.0.FromMeter();
+
+            if (Locked || previewRoomMaterials.Length == 0 || previewRoomMaterials[0] == null)
+            {
+                base.DrawViewportMeshes(args);
+                return;
+            }
+
+            Point3d cameraLocation = Rhino.RhinoDoc.ActiveDoc.Views.ActiveView.ActiveViewport.CameraLocation;
+
             for (int i = 0; i < lines.Count; i++)
             {
+
+                if (lines[i].From.DistanceTo(cameraLocation) > threshold) continue;
+                if (lines[i].To.DistanceTo(cameraLocation) > threshold) continue;
                 args.Display.DrawLine(lines[i], lineColors[i], 2);
                 //args.Display.DrawPatternedLine(line, Color.Brown, 0x001000111, 2);
 
@@ -220,7 +286,24 @@ namespace MantaRay.Components
                 }
             }
 
-            base.DrawViewportMeshes(args);
+            for (int i = 0; i < normalLines.Count; i++)
+            {
+                if (normalLines[i].From.DistanceTo(cameraLocation) > threshold) continue;
+                if (normalLines[i].To.DistanceTo(cameraLocation) > threshold) continue;
+                args.Display.DrawArrow(normalLines[i], normalColors[i], 0.0, 0.3);
+
+            }
+            double textHeight = 0.08.FromMeter();
+
+            for (int i = 0; i < textTags.Count; i++)
+            {
+                if (textTags[i].Item1.Origin.DistanceTo(cameraLocation) > threshold) continue;
+                args.Display.Draw3dText(textTags[i].Item3, textTags[i].Item2, textTags[i].Item1, textHeight, "Arial", false, false,
+                    Rhino.DocObjects.TextHorizontalAlignment.Center, Rhino.DocObjects.TextVerticalAlignment.Middle);
+                
+            }
+
+
 
         }
 
